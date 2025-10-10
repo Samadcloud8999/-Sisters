0d953b63e92385c5e012fff0528382795519094d
Samandar <samandartolybaev2@gmail.com>
2025-10-10 14:49:12 +0300

    version 2.0

--- src/components/Cakes/AdultsCakes.jsx
+++ src/components/Cakes/AdultsCakes.jsx
@@ -22,7 +22,7 @@ const AdultsCakes = () => {
             key={i}
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.7 }}
             viewport={{ once: true }}
           >
             <Card imageSrc={image.src} alt={image.alt} onClick={() => setSelectedImage(image.src)} />
